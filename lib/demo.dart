9963973f2d6d4d49cef89c894765300a0d4b3b80
superpucy@gmail.com <superpucy@gmail.com>
2019-12-14 14:01:15 +0800

    取得陀螺儀的值

--- lib/demo.dart
+++ lib/demo.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io' show Platform;
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +6,7 @@ import 'package:flutter/material.dart';
 //import 'package:firebase_database/firebase_database.dart';
 //import 'package:stretch/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:stretch/main.dart';
-import 'package:stretch/package_detail.dart';
+import 'package:sensors/sensors.dart';
 
 class DemoPage extends StatefulWidget {
   @override
@@ -15,16 +15,82 @@ class DemoPage extends StatefulWidget {
 
 class _DemoPageState extends State<DemoPage> {
 
-
+  List<double> _accelerometerValues;
+  List<double> _userAccelerometerValues;
+  List<double> _gyroscopeValues;
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+  <StreamSubscription<dynamic>>[];
 
 
   @override
   void initState() {
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        _accelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions.add(gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions
+        .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final List<String> accelerometer =
+    _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
+    final List<String> gyroscope =
+    _gyroscopeValues?.map((double v) => v.toStringAsFixed(1))?.toList();
+    final List<String> userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        ?.toList();
+
+
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+
+        Padding(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Accelerometer: $accelerometer'),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+        ),
+        Padding(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('UserAccelerometer: $userAccelerometer'),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+        ),
+        Padding(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Gyroscope: $gyroscope'),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+        ),
+      ],
+    );
   }
+
+
+
 }
